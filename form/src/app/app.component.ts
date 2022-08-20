292f87df47309785650ba9c037668dd3f2044bdf
caobianco1996 <caiocaobianco1@hotmail.com>
2022-08-20 18:58:25 -0300

    Trazer dados, e reset de form

--- form/src/app/app.component.ts
+++ form/src/app/app.component.ts
@@ -11,6 +11,15 @@ export class AppComponent {
   defaultQuestion = 'pet';
   answer = '';
   genders = ['male', 'female'];
+  user = {
+  username: '',
+  email: '',
+  secretQuestion: '',
+  answer: '',
+  gender: '',
+  };
+
+  submitted = false;
   
   suggestUserName() {
     const suggestedName = 'Superuser';
@@ -35,6 +44,13 @@ export class AppComponent {
   // }
 
   onSubmit(){
-    console.log(this.signUpForm);
+    this.submitted = true;
+    this.user.username = this.signUpForm.value.userData.username;
+    this.user.email = this.signUpForm.value.userData.email;
+    this.user.secretQuestion = this.signUpForm.value.secret;
+    this.user.answer = this.signUpForm.value.questionAnswer;
+    this.user.gender = this.signUpForm.value.gender;
+
+    this.signUpForm.reset();
   }
 }
